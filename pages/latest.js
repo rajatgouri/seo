c3ec343c83b0c1ec35ae0435f9c6636ad6c24e4b
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-10 17:36:29 +0530

    categories

--- pages/latest.js
+++ pages/latest.js
@@ -24,13 +24,12 @@ const latest = ({ posts }) => {
         url={`${siteMetadata.siteUrl}/latest`}
       />
       <Search/>
-        <div>
             <div className="divide-y divide-gray-200 dark:divide-gray-700">
                 <div className="pt-6 pb-8 space-y-2 md:space-y-5">
                     <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                         Latest
                     </h1>
-                    <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+                    <p className="text-lg leading-7 hy text-gray-500 dark:text-gray-400">
                         {siteMetadata.description}
                     </p>
                 </div>
@@ -99,7 +98,6 @@ const latest = ({ posts }) => {
           </Link>
                 </div>
             )}
-        </div>
         </>
     )
 }
