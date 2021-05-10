691e5f0e8b603e2e77eded9b40a2226a8df433bf
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-10 18:44:51 +0530

    toggle function

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,32 +1,44 @@
 import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
   // When mounted on client, now we can show the UI
-  useEffect(() => setMounted(true), [])
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  const handleToggle = () => {
+    alert('theme wil change')
+  }
+  
 
   return (
     <>
-      <label
-        for="toogle"
-        className="flex items-center cursor-pointer px-4">
-        <div className="relative">
-          <input id="toogle" type="checkbox" className="sr-only" />
-          <div className="w-10 h-4 bg-gray-400 rounded-full shadow-lg"></div>
-          <div className="dot absolute w-6 h-6 bg-white rounded-full shadow-lg -left-1 -top-1 transition"></div>
-        </div>
-      </label>
-      <style jsx>
-        {`
-        input:checked ~ .dot {
-          transform: translateX(100%);
-          background-color: #48bb78;
-        }
-        `}
-      </style>
+      <div className="flex items-center cursor-pointer px-4">
+        {/* tommorow start from here... */}
+        <label for="toggle" className="cursor-pointer" onClick={handleToggle} >
+          <div className="flex items-center">
+            <span className="mr-1">
+              <FontAwesomeIcon icon={faSun} />
+            </span>
+            <input type="checkbox" id="toggle" className="sr-only" />
+            <div className="w-14 h-8 flex items-center bg-gray-400 rounded-full p-1">
+              <div className="circle w-6 h-6 bg-white rounded-full shadow-md"></div>
+            </div>
+            <span className="text-sm">
+              <span className="ml-1">
+                <FontAwesomeIcon icon={faMoon} />
+              </span>
+            </span>
+          </div>
+        </label>
+      </div>
     </>
   )
 }
